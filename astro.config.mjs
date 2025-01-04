b489b25e8ed6f81ab91e9511a40d5857d8cd0487
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-04 21:01:21 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jackplowman.github.io',
-  base: 'repo-overseer',
-})
+  site: "https://jackplowman.github.io",
+  base: "repo-overseer",
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
