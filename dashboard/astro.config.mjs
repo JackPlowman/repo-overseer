157cc2eb2e2e7d8efe7cd5b4d277bc81f5dae516
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-18 18:29:42 +0000

    update

--- dashboard/astro.config.mjs
+++ dashboard/astro.config.mjs
@@ -5,13 +5,18 @@ import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jackplowman.github.io",
   base: "repo-overseer",
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }), robotsTxt(), sitemap()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    robotsTxt(),
+    sitemap(),
+  ],
 })
