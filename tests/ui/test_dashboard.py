022fbba23ddfe3ddce0da7ff3f0af7d44efe60f5
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-19 11:24:25 +0000

    test: Add Sitemap Tests (#108)
    # Pull Request
    
    ## Description
    
    This pull request includes changes to improve the test suite by adding
    new tests, updating dependencies, and refactoring constants. The most
    important changes include adding new tests for the sitemap, updating the
    `pyproject.toml` file to include a new dependency, and refactoring
    constants for better maintainability.
    
    ### New tests and updates:
    
    *
    [`tests/ui/test_dashboard.py`](diffhunk://#diff-0f5c761cdc6013814735e5a86f696efa96ebec90d540a4517d2deb396fdefbf7R2-R33):
    Added new tests `test_sitemap_index` and `test_sitemap` to check the
    accessibility of the sitemap index and sitemap.
    *
    [`tests/ui/test_dashboard.py`](diffhunk://#diff-0f5c761cdc6013814735e5a86f696efa96ebec90d540a4517d2deb396fdefbf7R2-R33):
    Imported the `requests` library to support the new tests.
    
    ### Dependency updates:
    
    *
    [`tests/pyproject.toml`](diffhunk://#diff-03049af3cb225ee5c5f22bc10731df89191cc762ad9ca9aa4cb7d3a87786ac8dL5-R5):
    Added `requests==2.32.3` to the dependencies list.
    
    ### Refactoring:
    
    *
    [`tests/ui/utils/constants.py`](diffhunk://#diff-ffd2fa12c0a17c2250e4cebd95863d383a45bb9190556d24a4bff98568d1562bL1-R4):
    Refactored constants by introducing `REPOSITORY_PREFIX` and updating
    `DASHBOARD_URL`, `SITEMAP_INDEX_URL`, and `SITEMAP_0_URL` to use this
    prefix.
    
    ### Configuration changes:
    
    *
    [`tests/pyproject.toml`](diffhunk://#diff-03049af3cb225ee5c5f22bc10731df89191cc762ad9ca9aa4cb7d3a87786ac8dL29-R29):
    Updated the ignore list to replace `SIM112` with `S101`.
    
    Fixes #96

--- tests/ui/test_dashboard.py
+++ tests/ui/test_dashboard.py
@@ -1,11 +1,34 @@
 from playwright.sync_api import Page, expect
+from requests import get
 
-from .utils.constants import DASHBOARD_URL
+from .utils.constants import DASHBOARD_URL, SITEMAP_0_URL, SITEMAP_INDEX_URL
 
 
-def test_dashboard_has_title(page: Page) -> None:
+def test_has_title(page: Page) -> None:
     """Check that the dashboard has the correct title."""
     # Act
     page.goto(DASHBOARD_URL)
     # Assert
     expect(page).to_have_title("Astro Basics")
+
+
+def test_sitemap_index() -> None:
+    """Check that the sitemap index is accessible."""
+    # Act
+    response = get(SITEMAP_INDEX_URL, timeout=5)
+    # Assert
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == "application/xml"
+    assert response.text.startswith('<?xml version="1.0" encoding="UTF-8"?>')
+    assert response.text.endswith("</sitemapindex>")
+
+
+def test_sitemap() -> None:
+    """Check that the sitemap is accessible."""
+    # Act
+    response = get(SITEMAP_0_URL, timeout=5)
+    # Assert
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == "application/xml"
+    assert response.text.startswith('<?xml version="1.0" encoding="UTF-8"?>')
+    assert response.text.endswith("</urlset>")
--- tests/ui/utils/constants.py
+++ tests/ui/utils/constants.py
@@ -1,1 +1,4 @@
-DASHBOARD_URL = "https://jackplowman.github.io/repo-overseer/"
+REPOSITORY_PREFIX = "https://jackplowman.github.io/repo-overseer"
+DASHBOARD_URL = f"{REPOSITORY_PREFIX}/"
+SITEMAP_INDEX_URL = f"{REPOSITORY_PREFIX}/sitemap-index.xml"
+SITEMAP_0_URL = f"{REPOSITORY_PREFIX}/sitemap-0.xml"
