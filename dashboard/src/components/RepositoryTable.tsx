189d90fe6b8195305bfa827158c7bc53a1be46cf
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-29 13:06:09 +0000

    update

--- dashboard/src/components/RepositoryTable.tsx
+++ dashboard/src/components/RepositoryTable.tsx
@@ -1,7 +1,19 @@
 import { columns } from "@/components/columns"
 import { DataTable } from "@/components/data-table"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { repositories } from "@/data/repositories.json"
 
 export default function RepositoryTable() {
-  return <DataTable columns={columns} data={repositories} />
+  return (
+    <Tabs defaultValue="details"className="w-full">
+      <TabsList>
+        <TabsTrigger value="details">Details</TabsTrigger>
+        <TabsTrigger value="security">Security</TabsTrigger>
+      </TabsList>
+      <TabsContent value="details">
+        <DataTable columns={columns} data={repositories} />
+      </TabsContent>
+      <TabsContent value="security">Put content here</TabsContent>
+    </Tabs>
+  )
 }
