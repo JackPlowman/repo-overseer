565dfcdb495afcb98ca19ad087fa1680ac788833
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-31 21:10:35 +0000

    build: Add Security Table (#144)
    # Pull Request
    
    ## Description
    
    This pull request focuses on enhancing the repository detail and
    security views in the dashboard by introducing new components and
    updating existing ones. The most important changes include renaming and
    restructuring columns, creating a new component for security columns,
    and updating the repository table to use these new components.
    
    Changes to components and columns:
    
    * Renamed `columns` to `RepositoryDetailColumns` in
    `dashboard/src/components/RepositoryDetailColumns.tsx` and updated the
    file name from `columns.tsx` to `RepositoryDetailColumns.tsx` for better
    clarity.
    * Created a new component `RepositorySecurityColumns` in
    `dashboard/src/components/RepositorySecurityColumns.tsx` to define the
    columns for the security view, including headers and cell renderers for
    repository name, secret scanning push protection, secret scanning, and
    dependabot security updates.
    
    Updates to repository table:
    
    * Updated `dashboard/src/components/RepositoryTable.tsx` to import and
    use the newly defined `RepositoryDetailColumns` and
    `RepositorySecurityColumns` components, and renamed the component to
    `RepositoryDetailTable` to reflect its purpose more accurately.
    
    Fixes #143

--- dashboard/src/components/RepositoryTable.tsx
+++ dashboard/src/components/RepositoryTable.tsx
@@ -1,19 +1,22 @@
-import { columns } from "@/components/columns"
 import { DataTable } from "@/components/data-table"
+import { RepositoryDetailColumns } from "@/components/RepositoryDetailColumns"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { repositories } from "@/data/repositories.json"
+import { RepositorySecurityColumns } from "./RepositorySecurityColumns"
 
-export default function RepositoryTable() {
+export default function RepositoryDetailTable() {
   return (
     <Tabs defaultValue="details" className="w-full">
-      <TabsList>
+      <TabsList className="w-full">
         <TabsTrigger value="details">Details</TabsTrigger>
         <TabsTrigger value="security">Security</TabsTrigger>
       </TabsList>
       <TabsContent value="details">
-        <DataTable columns={columns} data={repositories} />
+        <DataTable columns={RepositoryDetailColumns} data={repositories} />
       </TabsContent>
-      <TabsContent value="security">Put content here</TabsContent>
+      <TabsContent value="security">
+        <DataTable columns={RepositorySecurityColumns} data={repositories} />
+      </TabsContent>
     </Tabs>
   )
 }
