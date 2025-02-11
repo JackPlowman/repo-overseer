efceaafce33ee88caeada0581897428e0161f76a
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-02-11 21:28:18 +0000

    update

--- tests/ui/test_details.py
+++ tests/ui/test_details.py
@@ -1,7 +1,8 @@
 """Tests for the details table on the dashboard page."""
 
-from typing import Self
 from time import sleep
+from typing import Self
+
 import pytest
 from playwright.sync_api import Page
 
@@ -13,10 +14,10 @@ class TestDetailsTable:
 
     @pytest.mark.wip
     @pytest.mark.parametrize(
-        ("column", "column_position"), [("Pull Requests", 0), ("Issues", 1)]
+        ("test_id", "column_position"), [("pull-requests", 1), ("issues", 2)]
     )
     def test_column_sort(
-        self: Self, column: str, column_position: int, page: Page
+        self: Self, test_id: str, column_position: int, page: Page
     ) -> None:
         """Check that the column can be sorted."""
         # Arrange
@@ -27,27 +28,22 @@ class TestDetailsTable:
         page.locator("text=Details").click()
         # Wait for table to be loaded initially
         page.wait_for_selector("tbody tr")
-        # Get the repository column header button
-        page.get_by_role("button", name="Pull Requests").click()
-        sleep(60)
+        # Get the repository column header button and click it
+        page.get_by_test_id("pull-requests").click()
+        page.wait_for_selector("tbody tr")
+        # Wait for the table to be sorted
+        sleep(1)
+        rows = page.locator("tbody tr")
 
-        # Act & Assert - First click (ascending)
-        # sort_button.click()
-        # # Wait for the table to be sorted
-        # sleep(1)
-        # # Get all rows in the table
-        # rows = page.locator("tbody tr")
-        # # Iterate through the rows and compare values
-        # first_value = rows.nth(0).locator("td").nth(column_position).text_content()
-        # second_value = rows.nth(1).locator("td").nth(column_position).text_content()
-        # raise Exception(first_value, second_value)
-        # for i in range(rows.count() - 1):
-        #     first_value = rows.nth(i).locator("td").nth(column_position).text_content()
-        #     # first_value = rows.nth(i).locator("td").nth(column_position).text_content()
-        #     # second_value = (
-        #     #     rows.nth(i + 1).locator("td").nth(column_position).text_content()
-        #     # )
-        #     # assert first_value >= second_value, (
-        #     #     f"Row {i} value {first_value} is not less than or equal to "
-        #     #     f"row {i + 1} value {second_value}"
-        #     # )
+        for i in range(rows.count() - 1):
+            first_row_value = int(
+                rows.nth(i).locator("td").nth(column_position).text_content()
+            )
+            second_row_value = int(
+                rows.nth(i + 1).locator("td").nth(column_position).text_content()
+            )
+            print(first_row_value, second_row_value)
+            assert first_row_value >= second_row_value, (
+                f"Failed at row {i} and {i + 1}, {first_row_value=} and {second_row_value=}"
+            )
+
