38d7e49ec980b4bd9ae3434256afd84cf4a66c88
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-02-24 22:13:18 +0000

    update

--- tests/ui/test_common_table.py
+++ tests/ui/test_common_table.py
@@ -34,7 +34,7 @@ def test_table_sorting__repository_column(column: str, page: Page) -> None:
     first_repo.wait_for(state="visible")
     expect(first_repo).to_contain_text("useful-commands", timeout=5000)
 
-
+@pytest.mark.wip
 @pytest.mark.parametrize("column", ["Details", "Security", "Key Files"])
 def test_details_table_pagination(column: str, page: Page) -> None:
     """Test that the table can be paginated."""
@@ -49,11 +49,11 @@ def test_details_table_pagination(column: str, page: Page) -> None:
     page.wait_for_selector("tbody tr")
     # Get the next page button
     count = 0
-    while page.locator("text=Next").first.is_enabled():
+    while page.get_by_role("button", name="Next").get_attribute("disabled") is None:
         next_page_button = page.get_by_role("button", name="Next")
         count += 1
         next_page_button.click()
         # Wait for pagination to complete
         page.wait_for_selector("tbody tr")
     # Assert
-    assert count >= 1
+    assert count == 2
